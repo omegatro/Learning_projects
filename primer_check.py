Fixed Chi squared calculation - added division step and background value to avoid loosing peak due to oversignificance issue

primer_check.py
@@ -56,7 +56,7 @@ if args.fasta:
         
     #For each sequence combination - slide 2-mers from the split sequence along the reference sequence and compute sums of one-hot encodings for each aligned base (use index sliding)
     def slide_twomer(reference,twomer):
-        null_limit = 1.0000000000000001e-34
+        null_limit = 0.31 #1.0000000000000001e-34
         twomer_sum = np.asarray([0,0,0,0], dtype='object')
         for i,_ in enumerate(reference):
             if i == 0:
@@ -65,16 +65,27 @@ if args.fasta:
                 twomer_sum += 2*twomer[0]+reference[i-1]+twomer[-1]+2*reference[i]
             else:
                 twomer_sum += twomer[0]+reference[i]+twomer[-1]+reference[i-1]
-            aa_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([2,null_limit,null_limit,null_limit])))
-            ac_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,1,null_limit,null_limit])))
-            ag_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,null_limit,1,null_limit])))
-            at_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,null_limit,null_limit,1])))
-            cc_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,2,null_limit,null_limit])))
-            gc_arg = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,1,1,null_limit])))
-            ct_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,1,null_limit,1])))
-            gt_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,1,1])))
-            gg_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,2,null_limit])))
-            tt_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,null_limit,2])))
+        aa_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([2,null_limit,null_limit,null_limit])), np.asarray([2,null_limit/2,null_limit/2,null_limit/2])))
+        ac_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,1,null_limit,null_limit])), np.asarray([1,1,null_limit,null_limit])))
+        ag_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,null_limit,1,null_limit])), np.asarray([1,null_limit,1,null_limit])))
+        at_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,null_limit,null_limit,1])), np.asarray([1,null_limit,null_limit,1])))
+        cc_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,2,null_limit,null_limit])), np.asarray([null_limit/2,2,null_limit/2,null_limit/2])))
+        gc_arg = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,1,1,null_limit])), np.asarray([null_limit,1,1,null_limit])))
+        ct_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,1,null_limit,1])), np.asarray([null_limit,1,null_limit,1])))
+        gt_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,1,1])), np.asarray([null_limit,null_limit,1,1])))
+        gg_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,2,null_limit])), np.asarray([null_limit/2,null_limit/2,2,null_limit/2])))
+        tt_agr = np.sum(np.divide(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,null_limit,2])), np.asarray([null_limit/2,null_limit/2,null_limit/2,2])))
+        # aa_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([2,null_limit,null_limit,null_limit])))
+        # ac_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,1,null_limit,null_limit])))
+        # ag_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,null_limit,1,null_limit])))
+        # at_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([1,null_limit,null_limit,1])))
+        # cc_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,2,null_limit,null_limit])))
+        # gc_arg = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,1,1,null_limit])))
+        # ct_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,1,null_limit,1])))
+        # gt_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,1,1])))
+        # gg_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,2,null_limit])))
+        # tt_agr = np.sum(np.square(twomer_sum/np.amax(twomer_sum) - np.asarray([null_limit,null_limit,null_limit,2])))
+        
         return {'aa':aa_agr,'ac':ac_agr,'ag':ag_agr,'at':at_agr,"cc":cc_agr,'gc':gc_arg,'ct':ct_agr,'gt':gt_agr, 'tt':tt_agr, 'gg':gg_agr}
 
     #Compute phred scored p values for all sequence combinations
@@ -92,6 +103,7 @@ if args.fasta:
                 aggregate_sum[key] += twomer_dict[key]
         
         aggregate_sum = {key:[-10*log10(stats.chi2.pdf(value/len(twomers),9))] for key,value in aggregate_sum.items()}
+        # aggregate_sum = {key:[1-value/len(twomers)/max(aggregate_sum.values())] for key,value in aggregate_sum.items()}
         aggregate_sum["ref_id"] = [ref_id]
         aggregate_sum["split_id"] = [twomer_id]
         print(f"Finished: {aggregate_sum}")